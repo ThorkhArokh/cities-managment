dc1f1cad3eb441b3584f93b7fbe4389a1cbd4662
ThorkhArokh <jeremie.feugnet@gmail.com>
2026-04-25 12:19:14 +0200

    Merge version 1.0.2 (#29)
    * [DOC] #12 Features doc
    
    * chore: bump version to 1.0.1 [skip ci]
    
    * [FEAT] #26 Update buildings icons
    
    * [FEAT] #28 Edit city by default
    
    * [BUG] #27 Rename journal entry when rename city
    
    * [CI] Add auto milestone creation
    
    * [CI] Manual bump version to 1.0.2
    
    ---------
    
    Co-authored-by: Thorkh <thorkh.arokh@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- scripts/common/cm-settings.js
+++ scripts/common/cm-settings.js
@@ -2,8 +2,8 @@ import { MODULE_ID, IS_DEBUG_MODE, DATAS_STORE } from "./cm-constants.js"
 
 export const registerSystemSettings = function () {
     game.settings.register(MODULE_ID, IS_DEBUG_MODE, {
-        name: "Debug mode actif",
-        hint: "Activate debug mode",
+        name: "CM.settings.debugMode.name",
+        hint: "CM.settings.debugMode.hint",
         scope: "world",
         config: true,
         default: false,
@@ -11,11 +11,11 @@ export const registerSystemSettings = function () {
     })
 
     game.settings.register(MODULE_ID, "toggleCityEditMode", {
-        name: "Toggle city edit",
-        hint: "Activate city edit",
+        name: "CM.settings.toggleEdit.name",
+        hint: "CM.settings.toggleEdit.hint",
         scope: "world",
         config: true,
-        default: false,
+        default: true,
         type: Boolean,
     })
 
